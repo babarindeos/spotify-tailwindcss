629ae182b403e070c034fe53fb92e79e8a10a3aa
Seyibabs <seyibabs.ng@gmail.com>
2023-07-03 19:36:43 +0100

    sidebar complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,59 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#191624',
+        'meetup-blue' : '#00455D',
+        'meetup-purple' : '#1F24CC',
+        'gray-750': '#3f495a',
+        'gray-850' : '#222733',
+        'gray-900-spotify' : '#121212',
+        'gray-800-spotify' : '#181818',
+        'gray-700-spotify' : '#282828',
+      },
+      lineHeight: {
+        'extra-loose' : '2.5',
+      },
+      animation: {
+        slideup: 'slideup 1s ease-in-out',
+        slidedown: 'slidedown 1s ease-in-out',
+        slideleft: 'slideleft 1s ease-in-out',
+        slideright: 'slideright 1s ease-in-out',
+        wave: 'wave 1.2s linear infinite',
+        slowfade: 'slowfade 2.2s ease-in-out',
+      },
+      keyframes: {
+        slowfade: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        slideup: {
+          from: { opacity: 0, transform: 'translateY(25%)' },
+          to: { opacity: 1, transform: 'none' },
+        },
+        slidedown: {
+          from: { opacity: 0, transform: 'translateY(-25%)' },
+          to: { opacity: 1, transform: 'none' },
+        },
+        slideleft: {
+          from: { opacity: 0, transform: 'translateX(-20px)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+        slideright: {
+          from: { opacity: 0, transform: 'translateX(20px)' },
+          to: { opacity: 1, transform: 'translateX(0)' },
+        },
+        wave: {
+          '0%': { transform: 'scale(0)' },
+          '50%': { transform: 'scale(1)' },
+          '100%': { transform: 'scale(0)' },
+        },
+      },
+    },
+  },
+  variants: {
+      textColor: ['responsive', 'hover', 'focus', 'group-hover'],
   },
   plugins: [],
 }
